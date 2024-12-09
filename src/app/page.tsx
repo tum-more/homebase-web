e47fe1ebae35588910ca9a4f3e750be537f8f373
ratchaya.gun <ratchaya.gun@gmail.com>
2024-12-09 14:22:38 +0700

    feat: design token font-size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { Card } from "@/components/ui/card";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -10,7 +9,7 @@ export default function Home() {
       <p className="text-body1SemiBold font-body font-semibold text-red-500">
         This is body1 with semi-bold style.
       </p>
-      <Card className="shadow-drop-shadow-sm m-10">
+      <Card className="shadow-inner m-10">
         <p className="text-body1SemiBold font-body font-semibold text-red-500 px-16">
           This is body1 with semi-bold style.CardsddddThis is body1 with semi-bold style.CardsddddThis is body1 with semi-bold style.CardsddddThis is body1 with semi-bold style.CardsddddThis is body1 with semi-bold style.CardsddddThis is body1 with semi-bold style.CardsddddThis is body1 with semi-bold style.CardsddddThis is body1 with semi-bold style.CardsddddThis is body1 with semi-bold style.CardsddddThis is body1 with semi-bold style.Cardsdddd
         </p>
