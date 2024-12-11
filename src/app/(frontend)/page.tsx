f2609716c286edcd301cae92698d449f92b83ac4
ratchaya.gun <ratchaya.gun@gmail.com>
2024-12-11 16:04:33 +0700

    feat: #21798 add company list

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,121 +1,73 @@
+"use client";
+
 import { CompanyCard } from "@/components/organisms";
-import { Button, Card } from "@/components/ui/";
-import Image from "next/image";
+import {
+  getCompanies,
+  getCompanyTotalPagination,
+} from "@/lib/feature/company/company.action";
+import { CompanyWithIndustry } from "@/lib/feature/company/company.schema";
+import {
+  LoadingProvider,
+  useLoading,
+} from "@/share/providers/loadingContextProvider";
+import { useEffect, useState } from "react";
+
+function HomeContent() {
+  const { loading, setLoading, error, setError } = useLoading();
+  const [companies, setCompanies] = useState<CompanyWithIndustry[]>([]);
+  const [totalPages, setTotalPages] = useState<number>(0);
+
+  useEffect(() => {
+    async function fetchCompanies() {
+      try {
+        setLoading(true);
+
+        const data = await getCompanies();
+        setCompanies(data);
+
+        const pages = await getCompanyTotalPagination();
+        setTotalPages(pages);
+        console.log("Total Pages:", pages);
+      } catch (error) {
+        setError("Failed to fetch data");
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchCompanies();
+  }, []);
+
+  return (
+    <>
+      <div className="container mx-auto max-w-screen-xl">
+        {!loading && (
+          <>
+            <div className="flex flex-col gap-4">
+              {companies.map((company) => (
+                <CompanyCard
+                  key={`company-${company.companyName}`}
+                  data={company}
+                />
+              ))}
+            </div>
+            <div className="mt-4 text-center">
+              <p className="text-gray-700">
+                Total Pages: <span className="font-bold">{totalPages}</span>
+              </p>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
 
 export default function Home() {
   return (
-    <div>
-      <div
-        className="relative w-full sm:h-[calc(100vh-51px)] h-[calc(100vh-71px)] bg-cover bg-center flex items-center justify-center"
-        style={{ backgroundImage: "url('/images/mock/mock-banner.png')" }}
-      >
-        <h1 className="text-white text-4xl">Welcome</h1>
-      </div>
-
-      <CompanyCard />
-
-      <div className="container mx-auto max-w-screen-xl text-start">
-        <h1 className="text-heading-7-semi-bold">Hello</h1>
-        <h2 className={"text-orange-500"}>hhhhhhhh;;;;;</h2>
-        <p>This is a paragraph with the Inter font.</p>
-        <p className="font-body text-red-500 text-body-3-semi-bold">
-          This is body1 with semi-bold style.
-        </p>
-        <Card className="shadow-inner m-10">
-          <p className="text-red-500 px-16 text-body-5">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply
-            dummy text of the printing and typesetting industry. Lorem Ipsum has
-            been the industry's standard dummy text ever since the 1500s, when
-            an unknown printer took a galley of type and scrambled it to make a
-            type specimen book. It has survived not only five centuries, but
-            also the leap into electronic typesetting, remaining essentially
-            unchanged. It was popularised in the 1960s with the release of
-            Letraset sheets containing Lorem Ipsum passages, and more recently
-            with desktop publishing software like Aldus PageMaker including
-            versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the
-            printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown
-            printer took a galley of type and scrambled it to make a type
-            specimen book. It has survived not only five centuries, but also the
-            leap into electronic typesetting, remaining essentially unchanged.
-            It was popularised in the 1960s with the release of Letraset sheets
-            containing Lorem Ipsum passages, and more recently with desktop
-            publishing software like Aldus PageMaker including versions of Lorem
-            Ipsum. Lorem Ipsum is simply dummy text of the printing and
-            typesetting industry. Lorem Ipsum has been the industry's standard
-            dummy text ever since the 1500s, when an unknown printer took a
-            galley of type and scrambled it to make a type specimen book. It has
-            survived not only five centuries, but also the leap into electronic
-            typesetting, remaining essentially unchanged. It was popularised in
-            the 1960s with the release of Letraset sheets containing Lorem Ipsum
-            passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is
-            simply dummy text of the printing and typesetting industry. Lorem
-            Ipsum has been the industry's standard dummy text ever since the
-            1500s, when an unknown printer took a galley of type and scrambled
-            it to make a type specimen book. It has survived not only five
-            centuries, but also the leap into electronic typesetting, remaining
-            essentially unchanged. It was popularised in the 1960s with the
-            release of Letraset sheets containing Lorem Ipsum passages, and more
-            recently with desktop publishing software like Aldus PageMaker
-            including versions of Lorem Ipsum.
-          </p>
-        </Card>
-        <div className="flex align-center py-20">
-          <Button
-            disabled
-            iconLeft={
-              <Image
-                src={"/images/icons/Chevron-left.png"}
-                alt=""
-                width={24}
-                height={24}
-              />
-            }
-          >
-            ADD
-          </Button>
-          <Button
-            disabled={false}
-            iconLeft={
-              <Image
-                src={"/images/icons/Chevron-left.png"}
-                alt=""
-                width={24}
-                height={24}
-              />
-            }
-          >
-            Label
-          </Button>
-          <Button variant={"outline"} loading={false}>
-            Label
-          </Button>
-          <Button disabled variant={"outline"} loading={false}>
-            Label
-          </Button>
-          <Button variant={"brand"} loading={false}>
-            Get started
-          </Button>
-          <Button disabled={true} variant={"brand"} loading={false}>
-            Get started
-          </Button>
-          <Button variant={"text"} loading={false}>
-            Label
-          </Button>
-          <Button disabled variant={"text"} loading={false}>
-            Label
-          </Button>
-        </div>
-      </div>
-    </div>
+    <LoadingProvider>
+      <HomeContent />
+    </LoadingProvider>
   );
 }
--- src/components/organisms/CompanyCard.tsx
+++ src/components/organisms/CompanyCard.tsx
@@ -1,11 +1,31 @@
-import { Company } from "@/share/models/company.model";
+import { CompanyWithIndustry } from "@/lib/feature/company/company.schema";
 
 interface Props {
-  data?: Company;
+  data: CompanyWithIndustry;
   className?: string;
   onPress?: () => void;
 }
 
 export function CompanyCard(props: Props) {
-    return <div>Company</div>
+  return (
+    <div className="px-6 py-8 border border-[#DCDCDC] rounded-[12px] text-start">
+      <h2 className="text-heading-7-semi-bold !normal-case">
+        {props.data.companyName}
+      </h2>
+      <div className="flex sm:flex-row flex-col justify-between text-start mt-6">
+        <div className="flex-1">
+          <h3 className="text-heading-8-semi-bold !normal-case">Industry</h3>
+          <p className="text-gray-600">
+            {!!props.data.industry && props.data.industry?.length > 0
+              ? props.data.industry.join(", ")
+              : "-"}
+          </p>
+        </div>
+        <div className="flex-1">
+          <h3 className="text-heading-8-semi-bold !normal-case">Location</h3>
+          <p className="text-gray-600">{props.data.location ?? "-"}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
