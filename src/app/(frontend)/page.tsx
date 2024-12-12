eff66431f1728db258784dbe7c9304388637ea5a
ohm <ohm@morestudio.co.th>
2024-12-12 15:10:40 +0700

    feat(home page): clean code

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui";
 import { LoadingProvider } from "@/share/providers/loadingContextProvider";
 import CompanyListView from "./CompanyListView";
 import Image from "next/image";
@@ -9,19 +9,12 @@ export default function Home() {
   return (
     <LoadingProvider>
       <div>
-        <div className="relative w-full sm:h-[calc(100vh-71px)] bg-cover bg-center flex justify-between pt-6">
-          <div className="container mx-auto max-w-screen-xl text-start flex sm:flex-row flex-col items-center">
-            <div
-              className="pb-12"
-              >
-              <p
-                className="sm:text-heading-2 text-heading-4-bold bold pb-2"
-              >
+        <div className="container mx-auto max-w-screen-xl overflow-hidden relative w-full sm:h-[calc(100vh-51px)] flex flex-col sm:pt-0 pt-6 text-start sm:flex-row justify-between items-center">
+            <div className="sm:pb-0 pb-12 max-w-[526px]">
+              <p className="sm:text-heading-2 text-heading-4-bold bold pb-2">
                 Get to know your customers with forms worth filling out
               </p>
-              <p
-                className="text-body-3 text-gray-700 pb-8"
-              >
+              <p className="text-body-3 text-gray-700 pb-8">
                 Collect all the data you need to understand customers with forms
                 designed to be refreshingly different
               </p>
@@ -41,12 +34,12 @@ export default function Home() {
                 Get Started
               </Button>
             </div>
-            <img
+            <Image
               src="/images/Fixed-aspect-ratio-spacer.png"
               alt="get-product-certificate"
-              className="w-[588px]"
+              width={588}
+              height={588}
             />
-          </div>
         </div>
       </div>
       <CompanyListView />
