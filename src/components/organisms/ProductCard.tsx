393820cb93e909741eeb6c8f4a78afde08c02453
ratchaya.gun <ratchaya.gun@gmail.com>
2024-12-17 15:33:37 +0700

    fix(ProductCard): fix display image

--- src/components/organisms/ProductCard.tsx
+++ src/components/organisms/ProductCard.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { Product } from "@/lib/feature/categorize/categorize-data.schema";
 import Image from "next/image";
 import { Badge } from "../ui/badge";
 import { toDateFormat } from "@/share/helper/formatter";
+import { useState } from "react";
 
 interface Props {
   product?: Product;
@@ -11,10 +13,7 @@ interface Props {
 
 export function ProductCard(props: Props) {
   const { product, className, onPress } = props;
-
-  const imageLoader = (src: string) => {
-    return src;
-  };
+  const [failedImg, setFailedImg] = useState<boolean>(false);
 
   const handleOnPress = () => {
     if (onPress) {
@@ -28,26 +27,24 @@ export function ProductCard(props: Props) {
         className={`${className} px-6 py-8 border shadow-drop-shadow-base border-[#DCDCDC] rounded-[12px] text-start`}
       >
         <div className="flex flex-row justify-between text-start">
-          <div className="flex-1 sm:mr-4 mr-2">
+          <div className="flex-1 sm:mr-4 mr-2 min-h-[54px]">
             <h2 className="text-heading-7-semi-bold !normal-case line-clamp-2">
               {product?.productServiceName}
             </h2>
           </div>
-          <div className="w-[40px] h-[40px]">
-            <Image
-              loader={() =>
-                imageLoader(
-                  "https://thaicarbonlabel.tgo.or.th/admin/uploadfiles/approval/XL/ea468712d5.jpg"
-                )
-              }
-              src={`https://thaicarbonlabel.tgo.or.th/admin/uploadfiles/approval/XL/ea468712d5.jpg`}
-              alt={`product-${product?.productServiceName}`}
-              width={40}
-              height={40}
-              unoptimized
-              className="border rounded-lg !h-[40px] border-[#DCDCDC]"
-            />
-          </div>
+          {!failedImg && product?.productImageUrl && (
+            <div className="w-[40px] h-[40px]">
+              <Image
+                src={product.productImageUrl}
+                alt={`product-${product?.productServiceName}`}
+                width={40}
+                height={40}
+                onError={() => setFailedImg(true)}
+                unoptimized
+                className="border rounded-lg !h-[40px] border-[#DCDCDC] object-contain"
+              />
+            </div>
+          )}
         </div>
         <Badge variant="success" className="my-4">
           <span className="text-body-1 mr-2">{`{7.99}`}</span>kgCO2e
